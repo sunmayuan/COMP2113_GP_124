14b4159f5db3f75186defd67831c94927d4906ac
sunmayuan <100403570+sunmayuan@users.noreply.github.com>
2022-05-06 01:23:18 +0800

    Create welcome_main.cpp

---
+++ welcome_main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  cout << " *  /-/-/ *                                                           * /-/-/  *. "<< endl;
+  cout << "          *  /     /    *                            *    /     /   * " << endl;
+  cout << "        *  /   __-/  *                               * /-__  /  * " << endl;
+  cout << "           */    /  *                                      * /    /* " << endl;
+  cout << "              /*   /)/                                   /(/   */ " << endl;
+  cout << "              /// /    /            ___              /    / /// " << endl;
+  cout << "           //::/        /      *   *   *   *       /        /::// " << endl;
+  cout << "          ||:::|           / *        ^        *  /          '|:::|| " << endl;
+  cout << "          ||:::|              )* (        ) ))*(             ''|:::|| " << endl;
+  cout << "          ||:::|                ) )Õ  Õ((                  '|:::|| " << endl;
+  cout << "          ||:::|              (  (   ~_~    ))               ''|:::|| " << endl;
+  cout << "           //::/               ) (    ^    ))               /:::// " << endl;
+  cout << "            /Y/______ (     (     ( (    ______ /Y/ " << endl;
+  cout << "              v             /  /     )    )  ) /    ,  *¤  v , " << endl;
+  cout << "         , *  *  * ,      /      )  (   ( /     /,*            *, " << endl;
+  cout << "     , ''     ¤      '',  /        /    )  )        /  ¤       ,¤ ,  , " << endl;
+  cout << "              , ¤, ,  /            /( (            /    , *  ¤* , " << endl;
+  cout << "           , *       * ,  ,  ,  ,    Y    ,  ,  ,  ,/*   ¤          ¤*, " << endl;
+  cout << "              ¤      , *   ¤     * ,  , *  ¤          * , " << endl;
+  cout << "                                ¤  , *               ¤      *, " << endl;
+
+  cout << " Congradulations! You have been selected to be a wizard!" << endl;
+  cout << endl;
+  cout << endl;
+
+  struct Student {
+    string name;
+    int magic_power;
+    int house_credit;
+    int day;
+    string house;
+  };
+
+  while ( magic_power < 7 && house_credit >5){
+    if (day%5==0){
+      potion;
+      day++;
+    }
+    else if (day%5 == 1){
+      spell;
+      day++;
+    }
+    else if (day%5 == 2){
+      classpsyco;
+      day++;
+    }
+    else if (day%5 == 3){
+      tripgambler;
+      day++;
+    }
+    else if (day%5 == 4){
+      dragon;
+      day++;
+    }
+  }
+
+
+
+
+
+
+
+  cout << "Now! Let me introduce some rules to you, " << endl;
+  cout << "since you were in muggles' world for so long before!" << endl;
+  cout << endl;
+
+  cout << "You have to win 5 Magic power to graduate!" << endl;
+  cout << "You will stop you adventure if you lost 5 House Credit!" << endl;
+
+
+
+
+
+
+
+}
